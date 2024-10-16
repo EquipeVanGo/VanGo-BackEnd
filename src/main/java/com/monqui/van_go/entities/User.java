4d461367b37ca52c2346808dd3dbbe09c1a4ccbb
Érick Nunes <ericknp15@gmail.com>
2024-10-16 16:15:30 -0300

    feat: H2 database, test profile, JPA

--- src/main/java/com/monqui/van_go/entities/User.java
+++ src/main/java/com/monqui/van_go/entities/User.java
@@ -5,9 +5,19 @@ import java.util.Objects;
 
 import com.monqui.van_go.entities.location.Address;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String age;
 	private String name;
--- src/main/java/com/monqui/van_go/entities/location/Address.java
+++ src/main/java/com/monqui/van_go/entities/location/Address.java
@@ -3,9 +3,19 @@ package com.monqui.van_go.entities.location;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_address")
 public class Address implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String city;
 	private String state;
