17c4c7dc8f3cbb087bf9c2b427186db4997d93a8
Érick Nunes <ericknp15@gmail.com>
2024-12-04 16:31:51 -0300

    fix: fixed CorsConfig

--- src/main/java/com/monqui/van_go/config/CorsConfig.java
+++ src/main/java/com/monqui/van_go/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 	
-	@Bean
+	@Bean(name = "customCorsConfig")
 	public WebMvcConfigurer corsConfig() {
 		return new WebMvcConfigurer() {
 			@Override
